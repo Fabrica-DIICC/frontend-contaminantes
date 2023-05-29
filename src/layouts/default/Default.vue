b9f0dcdd08878b7000543406397ed1817fa0f3c0
Tomas Bravo <tomas.b.c@outlook.com>
2023-05-29 14:48:21 -0400

    layout con boton hacia atras

--- src/layouts/default/Default.vue
+++ src/layouts/default/Default.vue
@@ -1,12 +1,17 @@
 <template>
   <v-app>
-    <default-bar />
+    <Suspense>
+      <default-bar />
 
-    <default-view />
+      <default-view />
+      <template #fallback>
+        <v-progress-circular indeterminate></v-progress-circular>
+      </template>
+    </Suspense>
   </v-app>
 </template>
 
 <script setup>
-  import DefaultBar from './AppBar.vue'
-  import DefaultView from './View.vue'
+import DefaultBar from './AppBar.vue'
+import DefaultView from './View.vue'
 </script>
