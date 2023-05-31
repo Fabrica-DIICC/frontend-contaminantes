6bd9c0e94530cc1c8ddcd6ab7fe21559ed732874
Tomas Bravo <tomas.b.c@outlook.com>
2023-05-30 22:59:56 -0400

    agregando redireccion a home

--- src/router/index.js
+++ src/router/index.js
@@ -3,19 +3,20 @@ import { createRouter, createWebHistory } from "vue-router";
 //importar vistas
 const routes = [
   {
-    path: "/home",
-    name: "Home",
+    path: "/",
+    name: "init",
+    redirect: { path: "/home" },
     component: () => import('@/layouts/HomeLayout.vue'),
     children: [
       {
-        path: '',
-        name: 'init',
+        path: '/home',
+        name: 'Home',
         component: () => import('@/views/ViewHome.vue')
       }
     ]
   },
   {
-    path: '/',
+    path: '/init ',
     component: () => import('@/layouts/default/Default.vue'),
     children: [
       {
