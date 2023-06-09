7f24c5d2ea454091fc560ffb34807f9755864f59
Nacho <nacho@localhost.localdomain>
2023-06-09 19:00:09 +0000

    base solida

--- src/views/ViewInfo.vue
+++ src/views/ViewInfo.vue
@@ -9,39 +9,23 @@
     <v-container fluid class="">
         <v-row class="mt-10">
             <v-carousel height="750" hide-delimiter-background>
-                <v-carousel-item v-for="(slide, i) in slides" :key="i">
-                    <v-sheet :color="colors[i]" height="100%">
-                        <div class="d-flex fill-height justify-center align-center">
-                            <div class="text-h2">
-                                {{ slide }} Slide
-                            </div>
-                        </div>
-                    </v-sheet>
-                </v-carousel-item>
+                    <v-carousel-item>
+                        <v-sheet color="blue-grey-lighten-5" height="100%">
+                                <v-row>
+                                    <v-col cols="6">
+                                        <h2 class="text-h6 mt-8"><strong>No todo lo que come hace bien!</strong></h2>
+                                        <p class="mt-10"> Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+                                        <h2 class="text-h6 mt-10">Tranquilizate! no todo es tan malo</h2>                  
+                                        <p class="mt-10"> Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+                                    </v-col>
+                                </v-row>
+                        </v-sheet>
+                    </v-carousel-item>
             </v-carousel>
         </v-row>
     </v-container>
 </template>
 <script>
-  export default {
-    data () {
-      return {
-        colors: [
-          'blue-grey-lighten-5',
-          'blue-grey-lighten-5',
-          'blue-grey-lighten-5',
-          'blue-grey-lighten-5',
-          'blue-grey-lighten-5',
-        ],
-        slides: [
-          'First',
-          'Second',
-          'Third',
-          'Fourth',
-          'Fifth',
-        ],
-      }
-    },
-  }
+
 </script>
 
