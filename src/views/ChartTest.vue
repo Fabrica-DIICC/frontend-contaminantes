9a93b7e6a35637cd92c8b2b8e4996804c9f2f269
Primo18 <danilobo.elo@gmail.com>
2023-06-27 00:23:38 -0400

    add chart

---
+++ src/views/ChartTest.vue
@@ -0,0 +1,25 @@
+<template>
+    <div id="app">
+        <ChartCarousel />
+    </div>
+    <BarChart />
+</template>
+  
+<script>
+import BarChart from '../components/BarChart.vue'
+
+export default {
+    name: 'App',
+    components: {
+        BarChart,
+    },
+};
+</script>
+  
+<style>
+#app {
+    text-align: center;
+    padding: 20px;
+}
+</style>
+  
