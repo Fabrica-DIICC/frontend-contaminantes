42487db84c0a44315e7ae380e61f781acc82a367
Tomas Bravo <tomas.b.c@outlook.com>
2023-06-29 05:12:08 -0400

    creacion de loading component

---
+++ src/components/LoadingComponent.vue
@@ -0,0 +1,14 @@
+<template>
+    <v-container class="h-100">
+        <v-row justify="center" align-content="center" class="h-50">
+            <v-col class="text-center">
+                <v-progress-circular indeterminate size="100" width="8"></v-progress-circular>
+            </v-col>
+        </v-row>
+        <v-row align-content="center" class="h-25">
+            <v-col>
+                <h4 class="text-h4 text-center">Estamos Calculando la Informacion Pedida</h4>
+            </v-col>
+        </v-row>
+    </v-container>
+</template>
