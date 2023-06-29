cbc3ad05dc5e1348aeb9eec97a213776296aa7f1
Tomas Bravo <tomas.b.c@outlook.com>
2023-06-29 05:13:38 -0400

    creacion de vista de resultados informes

--- src/components/Generador/ResultadoGenerador.vue
+++ src/components/Generador/ResultadoGenerador.vue
@@ -1,6 +1,36 @@
 <template>
-    <BarChart></BarChart>
+    <v-container>
+        <v-row>
+            <h5 class="text-h5">Graficas de Peligro</h5>
+        </v-row>
+        <v-row>
+            <BarChart></BarChart>
+        </v-row>
+        <v-row>
+            <v-col>
+                <h5 class="text-h5">Datos de Consumo (100 primeros)</h5>
+            </v-col>
+        </v-row>
+        <v-row>
+            <v-col>
+                <v-btn variant="outlined" color="primary" rounded="" @click="downloadFile">Exportar Datos</v-btn>
+            </v-col>
+        </v-row>
+        <v-row>
+            <v-col>
+                <DataTable></DataTable>
+            </v-col>
+        </v-row>
+    </v-container>
 </template>
 <script setup>
+import { useAppStore } from '@/store/app';
 import BarChart from '../BarChart'
+import DataTable from '../DataTable.vue';
+const store = useAppStore()
+
+function downloadFile() {
+    window.open(store.fileOnApiUrl, '_blank', 'noreferrer');
+}
+console.log(import.meta.env.VITE_API_URL)
 </script>
