d9bc9c6ff15ff7de00a40e30dc7ad38af65ad18f
Jonathan Venegas Caro <jonvenega@gmail.com>
2023-06-29 06:15:28 -0400

    más gráficos

--- src/components/BarChart.vue
+++ src/components/BarChart.vue
@@ -20,6 +20,29 @@
             </v-container>
         </v-carousel-item>
     </v-carousel>
+
+    <v-carousel hide-delimiter-background height="500px" show-arrows="hover" delimiter-icon="mdi-circle-small">
+        <v-carousel-item>
+            <v-container class="fill-height">
+                <v-row class="h-100">
+                    <v-col>
+                        <Bar id="chart-16" :options="optionsChile" :data="chartDataChile2"></Bar>
+                    </v-col>
+                </v-row>
+            </v-container>
+        </v-carousel-item>
+
+        <v-carousel-item v-for="region in index" :key="region">
+            <v-container class="fill-height">
+                <v-row class="h-100">
+                    <v-col>
+                        <Bar :id="chart + region" :options="chartOptions[region]" :data="chartData2[region]"></Bar>
+                    </v-col>
+                </v-row>
+            </v-container>
+        </v-carousel-item>
+    </v-carousel>
+
 </template>
   
 <script setup>
@@ -36,6 +59,7 @@ const { regiones } = storeToRefs(store)
 const chartOptions = []
 const index = Array.from({ length: 15 }, (value, index) => index)
 const chartData = ref([])
+const chartData2 = ref([])
 const chartDataChile = {
 
     labels: ['As', "As i", "Cd", "Hg", "Pb"],
@@ -46,6 +70,16 @@ const chartDataChile = {
             backgroundColor: '#FF6384',
         }
     ]
+}
+const chartDataChile2 = {
+labels: ['As', "As i", "Cd", "Hg", "Pb"],
+datasets: [
+    {
+        label: 'Consumo Contaminantes',
+        data: [store.nacional.prom_contaminantes.As, store.nacional.prom_contaminantes['As i'], store.nacional.prom_contaminantes.Cd, store.nacional.prom_contaminantes.Hg, store.nacional.prom_contaminantes.Pb],
+        backgroundColor: '#FF6384',
+    }
+]
 }
 const optionsChile = {
     maintainAspectRatio: false,
@@ -82,6 +116,16 @@ onMounted(() => {
                 {
                     label: 'Tasa de Riesgo',
                     data: [regionObject.formula.As, regionObject.formula['As i'], regionObject.formula.Cd, regionObject.formula.Hg, regionObject.formula.Pb],
+                    backgroundColor: '#FF6384',
+                }
+            ]
+        })
+        chartData2.value.push({
+            labels: ['As', "As i", "Cd", "Hg", "Pb"],
+            datasets: [
+                {
+                    label: 'Consumo Contaminantes',
+                    data: [regionObject.prom_contaminantes.As, regionObject.prom_contaminantes['As i'], regionObject.prom_contaminantes.Cd, regionObject.prom_contaminantes.Hg, regionObject.prom_contaminantes.Pb],
                     backgroundColor: '#FF6384',
                 }
             ]
